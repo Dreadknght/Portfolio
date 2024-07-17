2233480fd56857c3e4cf826a34916748779fa588
Dreadknght <157273542+Dreadknght@users.noreply.github.com>
2024-07-17 22:56:18 +0800

    Add files via upload

---
+++ Portoflio/Assets/js/Portfolio.js
@@ -0,0 +1,8 @@
+function resume(){
+       window.open("https://dreadknght.github.io/rodeinopena.github.io/Resume - Opeña.pdf","_blank");
+}
+function DAS(){
+       window.open("https://dreadknght.github.io/rodeinopena.github.io/DAS-31G6_2023-2024.pdf","_blank");
+function Accomplishment(){
+       window.open("https://dreadknght.github.io/rodeinopena.github.io/Accomplishment Report - Opeña.pdf","_blank");
+}
